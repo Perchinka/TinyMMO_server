99bfd0da13a26ef4f89d16f450a6c63a16eb745f
Perchinka <alexsator.lukin@gmail.com>
2024-02-27 02:08:14 +0000

    Add basic consumer

--- services/chat_service/src/config.py
+++ services/chat_service/src/config.py
@@ -3,16 +3,11 @@ from src import logger
 
 class Config:
     LOGGING_LEVEL: str
-    
     KAFKA_URL: str
-    KAFKA_USERNAME: str
-    KAFKA_PASSWORD: str
-
+    
     def __init__(self):
         self.KAFKA_URL = os.environ.get('KAFKA_URL')
-        self.KAFKA_USERNAME = os.environ.get('KAFKA_USERNAME')
-        self.KAFKA_PASSWORD = os.environ.get('KAFKA_PASSWORD')
 
-        self.LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'INFO')
+        self.LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'DEBUG')
 
         logger.setup_logger(self.LOGGING_LEVEL)
--- services/chat_service/src/main.py
+++ services/chat_service/src/main.py
@@ -1,13 +1,13 @@
 from src.config import Config
 from confluent_kafka import Producer, Consumer
 from pydantic import BaseModel
-from datetime import datetime
 import socket
+import logging
+
 
 class ChatMsg(BaseModel):
+    chanel_id: int
     content: str
-    reciever: str
-    timestamp: str
 
 
 class KafkaAdapter:
@@ -18,21 +18,35 @@ class KafkaAdapter:
         })
         self.consumer = Consumer({
             'bootstrap.servers': config.KAFKA_URL,
-            'group.id': 'chat_service',
+            'group.id': 'chat-service',
         })
     
     def publish_msg(self, msg: ChatMsg):
-        self.producer.produce('snd', key=msg.reciever, value=msg.content)
+        self.producer.produce('snd', key=str(msg.chanel_id), value=msg.content)
+        logging.info('Message published: {}'.format(msg))
+    
+    def consume_msg(self):
+        self.consumer.subscribe(['rcv'])
+        while True:
+            raw_msg = self.consumer.poll(1.0)
+            if raw_msg is None:
+                continue
+            if raw_msg.error():
+                logging.error('Consumer error: {}'.format(raw_msg.error()))
+                continue
+
+            msg = ChatMsg(chanel_id=int(raw_msg.key().decode('utf-8')), content=raw_msg.value().decode('utf-8'))
+            logging.info('Received message: {}'.format(msg))
+            self.consumer.commit
     
 
-    
-    
 def main():
     config = Config()
     kafka = KafkaAdapter(config)
-    chat_msg = ChatMsg(content='Hello', reciever='user1', timestamp=datetime.now().isoformat())
+    chat_msg = ChatMsg(chanel_id=1, content='Hello')
     kafka.publish_msg(chat_msg)
     kafka.producer.flush()
+    kafka.consume_msg()
 
 if __name__ == '__main__':
     main()
