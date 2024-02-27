e799850b1aa003970d57469aabaeb2ef86367ef9
Perchinka <alexsator.lukin@gmail.com>
2024-02-27 14:53:32 +0000

    Add packets

--- services/client_interface/src/config.py
+++ services/client_interface/src/config.py
@@ -3,8 +3,10 @@ from src import logger
 
 class Config:
     LOGGING_LEVEL: str
+    KAFKA_URL: str
 
     def __init__(self):
-        self.LOGGING_LEVEL = os.environ.get('LOGING_LEVEL')
+        self.LOGGING_LEVEL = os.environ.get('LOGING_LEVEL', 'DEBUG')
+        self.KAFKA_URL = os.environ.get('KAFKA_URL')
 
         logger.setup_logger(self.LOGGING_LEVEL)
---
+++ services/client_interface/src/packets.py
@@ -0,0 +1,63 @@
+from enum import Enum
+import logging
+import json
+
+class PacketType(Enum):
+    Chat = 0
+
+class Packet:
+    def __init__(self, packet_type: PacketType, *payloads: tuple):
+        self.packet_type = packet_type
+        self.payloads: tuple = payloads
+    
+    def __str__(self) -> str:
+        serialized = {'a': self.packet_type.value}
+        for i, payload in enumerate(self.payloads):
+            serialized[f'p{i}'] = payload
+        data = json.dumps(serialized)
+        return data
+
+    def __bytes__(self) -> bytes:
+        return str(self).encode('utf-8')
+
+    
+def from_json(data: str) -> Packet:
+    deserialized = json.loads(data)
+    packet_type = PacketType(deserialized['a']).name
+    payloads = tuple(deserialized[f'p{i}'] for i in range(len(deserialized) - 1))
+
+    class_name = packet_type + 'Packet'
+    try:
+        constructor: type = globals()[class_name]
+        return constructor(*payloads)
+    except KeyError as e:
+        logging.error(f'Packet type {class_name} not found')
+        return None
+    except TypeError as e:
+        logging.error(f'{class_name} can\'t handle arguments {payloads}. Expected {constructor.__init__.__code__.co_varnames[1:]}.')
+        return None
+    except Exception as e:
+        logging.error(f'Error deserializing packet: {e}')
+        return None
+    
+
+class ChatPacket(Packet):
+    def __init__(self, to_id: int, from_id: int, content: str):
+        super().__init__(PacketType.Chat, to_id, from_id, content)
+        self.to_id = to_id
+        self.from_id = from_id
+        self.content = content
+    
+    def __str__(self) -> str:
+        return json.dumps({
+            'a': self.packet_type.value,
+            'p0': self.to_id,
+            'p1': self.from_id,
+            'p2': self.content
+        })
+    
+    def __bytes__(self) -> bytes:
+        return str(self).encode('utf-8')
+    
+    def __repr__(self) -> str:
+        return f'ChatPacket(to_id={self.to_id}, from_id={self.from_id}, content={self.content})'
